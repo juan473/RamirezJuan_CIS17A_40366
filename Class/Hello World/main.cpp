0b5e3ccaaf8878aec6860380fc61155b34be082a
juan473 <jramirez473@student.rccd.edu>
2020-02-24 16:46:55 -0800

    Signed-off-by: juan473 <jramirez473@student.rccd.edu>

---
+++ Class/Hello World/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Juan Ramirez
+ * Created on February 24, 2020, 1:29 PM
+ * Purpose: Hello World
+ */
+
+//System Level Libraries
+#include <iostream>   //I/O Library
+using namespace std;  //Libraries compiled under std
+
+//User Level Libraries
+
+//Global Constants - Science/Math Related
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) { 
+    //Random Number Seed Set Here
+    
+    //Variable Declarations
+    
+    //Variable Initialization
+    
+    //Mapping Process Inputs to Outputs
+    
+    //Display Outputs
+    cout << "Hello World" << endl; //All in 1 Line
+    cout << "Hello World"; //All in 2 Lines
+    cout << endl;
+    cout << "Hello "; //All in 3 Lines
+    cout << "World";
+    cout << endl;
+    
+    //Clean Up
+    
+    //Exit stage right!
+    return 0;
+}
